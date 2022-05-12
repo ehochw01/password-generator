801aad60202ab747cc9397e0f7d6d6e8cc4285fd
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-05-11 21:48:28 -0400

    Completed the generatePasswordfunction. Implemented an algorithm to build a password of specified length by selecting random characters from the charSet length times. Returns the password string.

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,6 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var criteria = passwordPrompt();
-  console.log(criteria);
   var password = generatePassword(criteria);
   var passwordText = document.querySelector("#password");
 
@@ -18,7 +17,7 @@ generateBtn.addEventListener("click", writePassword);
 
 //prompt for the length of the password. Must be between 8 and 128 characters
 function passwordPrompt() {
-  // creating a password criteria object that will contain all of the criteria the user chooses
+  // initialize a password criteria object that will contain all of the criteria the user chooses
   var criteria = {length:0, lowercase:false, uppercase:false, numeric:false, special:false};
   //prompt password length
   var length = prompt("How long do you want the password to be (choose a length between 8 and 128 characters");
@@ -31,7 +30,7 @@ function passwordPrompt() {
   }
 
   var atLeastOneCharacterTypeSelected = false;
-   // at least one character type muse be created
+   // at least one character type muse be selected by the user
   while (!atLeastOneCharacterTypeSelected){
       //confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
     if (confirm("Do you want to include lowercase characters?")) {
@@ -40,19 +39,18 @@ function passwordPrompt() {
     }
     if (confirm("Do you want to include uppercase characters?")) {
       criteria.uppercase = true;
-      atLeastOneCharacterTypeSelected = true
+      atLeastOneCharacterTypeSelected = true;
     }
     if (confirm("Do you want to include numeric characters?")) {
       criteria.numeric = true;
-      atLeastOneCharacterTypeSelected = true
+      atLeastOneCharacterTypeSelected = true;
     }
     if (confirm("Do you want to include special characters?")) {
       criteria.special = true;
       atLeastOneCharacterTypeSelected = true;
     }
-
     if (!atLeastOneCharacterTypeSelected) {
-      alert("You must select at least one type of character")
+      alert("You must select at least one type of character");
     }
   }
 
@@ -60,16 +58,15 @@ function passwordPrompt() {
 }
 
 function generatePassword(passCriteria) {
-  console.log(passCriteria);
   var charSet = "";
   const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
   const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const numbers = '123456789';
-  var specialFirstpart = ' !"#$%&'
-  var specialSecondpart = "')*+,-./:;<=>?@[\]^_`{|}~"
+  var specialFirstpart = ' !"#$%&';
+  var specialSecondpart = "')*+,-./:;<=>?@[\]^_`{|}~";
   const special =  specialFirstpart + specialSecondpart;
 
-  // create a string that contains the set of possible characters for password based on user-confirmed criteria
+  // create a string that contains the set of possible characters for the password based on the user-confirmed criteria
   if (passCriteria.lowercase) {
     charSet += lowerCase;
   }
@@ -83,8 +80,22 @@ function generatePassword(passCriteria) {
     charSet += special;
   }
 
-  console.log(charSet);
-
-
-
+  // convert concatenated string to an array
+  var charSetarray = charSet.split('');
+  var n = charSetarray.length;  
+  //start building the password out of random characters from the charSet
+  var passwordArray = [];
+  for (var i = 0; i < passCriteria.length; i++) {
+    var j = getRandomNum(n);
+    var char = charSetarray[j];
+    passwordArray[i] = char;
+  }
+  //turn the password char array into a string and return
+  return passwordArray.join('');
 }
+
+// a random number generation function found from the internet https://www.codespeedy.com/shuffle-characters-of-a-string-in-javascript/
+function getRandomNum(n) {
+  return Math.floor(Math.random() * n);
+}
+
